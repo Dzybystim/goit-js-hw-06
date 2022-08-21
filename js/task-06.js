426627087312ea749366d178b9c82d0f90d5bf32
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2022-08-21 14:45:24 +0300

    My finish

--- js/task-06.js
+++ js/task-06.js
@@ -5,11 +5,14 @@ const validator = (event) => {
    
     if (event.currentTarget.value.length !== Number(event.currentTarget.dataset.length)) {
         inputElement.classList.add("invalid");
+
+        inputElement.classList.remove("valid");
     
      } else {
 
         inputElement.classList.add("valid");
       
+        inputElement.classList.remove("invalid");  
      }
    
 }
